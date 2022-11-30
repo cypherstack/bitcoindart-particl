d178734245ecce49115ee870ea0a7745d100f7c7
sneurlax <sneurlax@gmail.com>
2022-11-30 15:01:18 -0600

    default to use particl network

--- lib/src/models/networks.dart
+++ lib/src/models/networks.dart
@@ -55,3 +55,11 @@ final particl = NetworkType(
     pubKeyHash: 0x38,
     scriptHash: 0x3c,
     wif: 0x6c);
+
+final particltestnet = NetworkType(
+    messagePrefix: '\x18Bitcoin Signed Message:\n',
+    bech32: 'tpw',
+    bip32: Bip32Type(public: 0xe1427800, private: 0x04889478),
+    pubKeyHash: 0x76,
+    scriptHash: 0x7a,
+    wif: 0x2e);
